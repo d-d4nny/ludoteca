a6a7907b11d303568135a481aebfc1a25fd5682d
d-d4nny <danitbp12@gmail.com>
2023-11-28 09:02:19 +0100

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    firebase: {"projectId":"ludoteca-5d95a","appId":"1:984709996137:web:cbd8615e67d3dc71070cf8","storageBucket":"ludoteca-5d95a.appspot.com","apiKey":"AIzaSyC7LiGy2JQEWS_Yq33bwZAfscq7R7CHsDQ","authDomain":"ludoteca-5d95a.firebaseapp.com","messagingSenderId":"984709996137"}
+};
